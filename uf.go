a89737a6d82b5914b1a4cc6aed9f8378fcf2e63c
diebels727 <diebelsalternative@hotmail.com>
2014-08-13 09:17:46 -0400

    Remove comments.

--- uf.go
+++ uf.go
@@ -70,18 +70,3 @@ func (u *UnionFind) PrettyPrintClusterIDs() {
     fmt.Println("Index in Clusters: ",index," Cluster ID: ",id)
   }
 }
-
-// func connected(pair Pair,ids []int) bool {
-//   return ids[pair.P] == ids[pair.Q]
-// }
-//
-// func union(pair Pair,ids []int) []int {
-//   logger.Printf("[union] called")
-//   if (connected(pair,ids)) { return ids };
-//   pid := ids[pair.P];
-//   for i:=0;i<len(ids);i++ {
-//       if (ids[i] == pid) { ids[i] = ids[pair.Q] };
-//   }
-//   logger.Printf("[union] finished")
-//   return ids
-// }
