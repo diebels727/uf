f38c7232a466acdbb04a497d3838bb17f6a69901
diebels727 <diebelsalternative@hotmail.com>
2014-08-13 09:12:37 -0400

    Union a few tuples.

--- uf_test.go
+++ uf_test.go
@@ -78,6 +78,39 @@ func TestTrivialUnionShouldConnectBothObjects(t *testing.T) {
   }
 }
 
+func TestComplexUnionShouldConnectObjects(t *testing.T) {
+  var testObjects []TestObject
+  p := TestObject{10}
+  q := TestObject{1}
+  r := TestObject{7}
+  s := TestObject{9}
+  v := TestObject{11}
+  w := TestObject{14}
+  testObjects = make([]TestObject,0)
+  testObjects = append(testObjects,p)
+  testObjects = append(testObjects,q)
+  testObjects = append(testObjects,r)
+  testObjects = append(testObjects,s)
+  testObjects = append(testObjects,v)
+  testObjects = append(testObjects,w)
+  u := UnionFind{}
+  u.Init(TestObjects(testObjects))
+  u.Union(p,q)
+  if u.Find(p) != u.Find(q) {
+    t.Error("Expected ",u.Find(p)," to equal ",u.Find(q))
+  }
+  u.Union(r,s)
+  if u.Find(r) != u.Find(s) {
+    t.Error("Expected ",u.Find(r)," to equal ",u.Find(s))
+  }
+  u.Union(v,w)
+  if u.Find(v) != u.Find(w) {
+    t.Error("Expected ",u.Find(v)," to equal ",u.Find(w))
+  }
+
+
+}
+
 
 
 
